675611fb26847461a6b971ff3f43953c25e09a32
LeeGodSRC <lee20040919@gmail.com>
2020-03-17 17:59:21 +0700

    Initial commit

---
+++ src/main/java/org/imanity/commandhandler/CommandHelp.java
@@ -0,0 +1,15 @@
+package org.imanity.commandhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommandHelp {
+
+	private String syntax;
+	private String description;
+
+}
